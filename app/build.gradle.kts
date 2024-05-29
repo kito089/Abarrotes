cc81fa22ba9f2fe18eaa87d7f60b73671805e711
yael <hiram060220@gmail.com>
2024-05-28 18:20:47 -0600

    xd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,14 @@
 plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
+    alias(libs.plugins.android.application)
 }
 
 android {
-    namespace = "com.example.abarrotes"
+    namespace = "com.example.examen3"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.abarrotes"
-        minSdk = 24
+        applicationId = "com.example.examen3"
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -31,11 +30,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
